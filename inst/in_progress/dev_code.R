bf8369611106270f9dadba83ef2acc8fe1d6481e
mikejohnson51 <jmj00@ucsb.edu>
2021-12-16 14:14:15 -0700

    add some dev code short hand

---
+++ inst/in_progress/dev_code.R
@@ -0,0 +1,11 @@
+devtools::document()
+
+####
+rcompendium::add_dependencies()
+rcompendium::add_dependencies_badge()
+rcompendium::add_github_actions_check()
+rcompendium::add_github_actions_check_badge()
+rcompendium::add_lifecycle_badge()
+####
+
+devtools::check(vignettes = FALSE)
